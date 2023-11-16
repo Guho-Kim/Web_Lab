f402d5c69d98dd91f2737267da0afe184305fef0
Guho-Kim <weeeeek2@naver.com>
2023-11-16 17:26:05 +0900

    20231116

---
+++ Assignment/Final_proj/HTML-Functions_Codes/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+/*localStorage.setItem("userColor", "red");
+localStorage.setItem("userfontsize", "50px");
+localStorage.removeItem('userfontsize');
+console.log(localStorage.getItem('userColor'));
+localStorage.clear();
+sessionStorage.setItem("userColor", "red");
+sessionStorage.setItem("userfontsize", "50px");
+sessionStorage.removeItem('userfontsize');
+console.log(sessionStorage.getItem('userColor'));
+sessionStorage.clear();*/
+var expireDate = new Date( Date.now() + (1000*60*60*24));
+document.cookie='color=red; expires='+expireDate.toUTCString();
+
+if(localStorage.getItem('websiteColor')!==null){
+
+$('body').css('background',localStorage.getItem('websiteColor'));
+
+}
+
+
+$('#colorpicker').focusout(function(){
+var color = $('#colorpicker').val();
+localStorage.setItem('websiteColor',color);
+$('body').css('background',localStorage.getItem('websiteColor'));
+
+});
+
+
+});
